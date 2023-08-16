788cffb2ce50ec5c28e5177dbb928282af1692f2
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-08-16 14:36:50 +0530

    close modal on clicking outside

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,7 +7,7 @@ import React, { useState } from 'react';
    // console.log(show)
     return ( <div>
      <button onClick={()=>setDisplay(true)}>Show Modal</button>
-     {display && <div className="model-overlay">
+     {display && <div className="model-overlay" onClick={() => setDisplay(false)}>
       <button onClick={() => setDisplay(false)} className="model-close">Close</button>
       <p className="model-p">This is the content of the modal</p>
       </div>}
