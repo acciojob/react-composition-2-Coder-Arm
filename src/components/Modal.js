bea38b72c76401e64157806e68d2501f399c996d
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-08-16 14:31:54 +0530

    push

---
+++ src/components/Modal.js
@@ -0,0 +1,18 @@
+ 
+import React, { useState } from 'react';
+
+
+ const Modal = ({show}) => {
+   const [display, setDisplay] = useState("");
+   // console.log(show)
+    return ( <div>
+     <button onClick={()=>setDisplay(true)}>Show Modal</button>
+     {display && <div className="model-overlay">
+      <button onClick={() => setDisplay(false)} className="model-close">Close</button>
+      <p className="model-p">This is the content of the modal</p>
+      </div>}
+    </div>
+    )
+ }
+
+ export default Modal
