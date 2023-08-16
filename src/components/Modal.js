4558e0efb30fa1564f0f0b328e3f65c6ef98deec
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-08-16 14:45:12 +0530

    changed classNames

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,9 +5,11 @@ import React, { useState } from 'react';
  const Modal = ({show}) => {
    const [display, setDisplay] = useState("");
    // console.log(show)
-    return ( <div>
+    return ( <div className='modal-overlay'>
+      <div>
      <button onClick={()=>setDisplay(true)}>Show Modal</button>
-     {display && <div className="model-overlay" onClick={() => setDisplay(false)}>
+     </div>
+     {display && <div className='content'>
       <button onClick={() => setDisplay(false)} className="model-close">Close</button>
       <p className="model-p">This is the content of the modal</p>
       </div>}
